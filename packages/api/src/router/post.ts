b22b95c0e52cec4ead4558c64bf06433b1fe03ef
Hugo Maltese <hugo.maltese@epitech.eu>
2022-12-06 11:46:55 -0500

    fix model validity

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,5 +1,6 @@
 import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
+import { PostType } from "@prisma/client";
 
 export const postRouter = router({
   all: publicProcedure.query(({ ctx }) => {
@@ -9,7 +10,18 @@ export const postRouter = router({
     return ctx.prisma.post.findFirst({ where: { id: input } });
   }),
   create: publicProcedure
-    .input(z.object({ title: z.string(), content: z.string() }))
+    .input(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+        desc: z.string(),
+        type: z.enum([PostType.RENTED, PostType.WAITING_FOR_RENT]),
+        price: z.number(),
+        duration: z.date(),
+        size: z.number(),
+        furnished: z.boolean(),
+      }),
+    )
     .mutation(({ ctx, input }) => {
       return ctx.prisma.post.create({ data: input });
     }),
