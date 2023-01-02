fcba97f1deb26aae1eea89126d7c532fcc464274
Hugo Maltese <hugo.maltese@epitech.eu>
2023-01-02 13:09:56 -0500

    fix test

--- packages/api/src/tests/post.test.ts
+++ packages/api/src/tests/post.test.ts
@@ -2,12 +2,13 @@ import { type inferProcedureInput } from "@trpc/server";
 import { createContextInner } from "../context";
 import { appRouter, type AppRouter } from "../router/index";
 import { describe, expect, test } from "vitest";
+import { PostType } from "@leace/db";
 
 describe("[Router][post]", () => {
   let id = "";
   const input: inferProcedureInput<AppRouter["post"]["create"]> = {
     createdBy: "",
-    type: "WAITING_FOR_RENT",
+    type: PostType.WAITING_FOR_RENT,
     title: "NicePost",
     content: "SomeContent",
     desc: "LittleDescription",
