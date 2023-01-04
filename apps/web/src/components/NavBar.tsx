d13526ad0d8bb7888b900c289359e3625a12befa
Théo Pomies <theo.pomies@outlook.com>
2023-01-04 03:10:58 -0500

    better navbar

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -1,6 +1,29 @@
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 
+const links = [
+  {
+    href: "/stack/candidats",
+    label: "Home",
+  },
+  {
+    href: "#",
+    label: "Dashboard",
+  },
+  {
+    href: "#",
+    label: "Matches",
+  },
+  {
+    href: "#",
+    label: "Profile",
+  },
+  {
+    href: "#",
+    label: "Notifications",
+  },
+];
+
 export function NavBar() {
   return (
     <div className="flex w-56 flex-col overflow-hidden bg-white">
@@ -8,45 +31,22 @@ export function NavBar() {
         <h1 className="text-3xl uppercase text-indigo-500">Leace</h1>
       </div>
       <ul className="flex flex-col py-4">
-        <li>
-          <Link
-            href="#"
-            className="flex h-12 transform flex-row items-center text-gray-500 transition-transform duration-200 ease-in hover:translate-x-2 hover:text-gray-800"
-          >
-            <span className="inline-flex h-12 w-12 items-center justify-center text-lg text-gray-400"></span>
-            <span className="text-sm font-medium">Dashboard</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="#"
-            className="flex h-12 transform flex-row items-center text-gray-500 transition-transform duration-200 ease-in hover:translate-x-2 hover:text-gray-800"
-          >
-            <span className="inline-flex h-12 w-12 items-center justify-center text-lg text-gray-400"></span>
-            <span className="text-sm font-medium">Matches</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="#"
-            className="flex h-12 transform flex-row items-center text-gray-500 transition-transform duration-200 ease-in hover:translate-x-2 hover:text-gray-800"
-          >
-            <span className="inline-flex h-12 w-12 items-center justify-center text-lg text-gray-400"></span>
-            <span className="text-sm font-medium">Profile</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="#"
-            className="flex h-12 transform flex-row items-center text-gray-500 transition-transform duration-200 ease-in hover:translate-x-2 hover:text-gray-800"
-          >
-            <span className="inline-flex h-12 w-12 items-center justify-center text-lg text-gray-400"></span>
-            <span className="text-sm font-medium">Notifications</span>
-            <span className="ml-auto mr-6 rounded-full bg-red-100 px-3 py-px text-sm text-red-500">
-              5
-            </span>
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li key={link.label}>
+            <Link
+              href={link.href}
+              className="flex h-12 transform flex-row items-center text-gray-500 transition-transform duration-200 ease-in hover:translate-x-2 hover:text-gray-800"
+            >
+              <span className="inline-flex h-12 w-12 items-center justify-center text-lg text-gray-400"></span>
+              <span className="text-sm font-medium">{link.label}</span>
+              {link.label === "Notifications" && (
+                <span className="ml-auto mr-6 rounded-full bg-red-100 px-3 py-px text-sm text-red-500">
+                  5
+                </span>
+              )}
+            </Link>
+          </li>
+        ))}
         <li>
           <Link
             href="#"
