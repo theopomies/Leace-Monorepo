9a93f4998a8bb795b0fe41c83b41c5d4fce9671e
Théo Pomies <theo.pomies@outlook.com>
2023-01-31 16:55:28 -0500

    add premium check

--- apps/web/src/pages/premium/stripeComponent.tsx
+++ apps/web/src/pages/premium/stripeComponent.tsx
@@ -1,12 +1,13 @@
 import Script from "next/script";
 
-export const StripePricingTable = () => {
+export const StripePricingTable = ({ userId }: { userId: string }) => {
   return (
     <>
       <Script async src="https://js.stripe.com/v3/pricing-table.js"></Script>
       <stripe-pricing-table
         pricing-table-id="prctbl_1MW5AVChNGYmKUwv92oc1cIJ"
         publishable-key="pk_test_51MW4eUChNGYmKUwvrzku5ORxQsj2I0czUzuIzCgFKQyWmS3tCDeeOKJzqvDaMVViOgehcDWCg4Nd9k90FjNEwlJ800McMK6MO6"
+        client-reference-id={userId}
       ></stripe-pricing-table>
     </>
   );
