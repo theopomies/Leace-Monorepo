9da9a5891f636f3a9939177fbcb165b21978f586
Hugo Maltese <hugo.maltese@epitech.eu>
2023-03-19 15:49:08 -0400

    need this unused var

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,6 +2,7 @@ import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default withClerkMiddleware((_req: NextRequest) => {
   return NextResponse.next();
 });
