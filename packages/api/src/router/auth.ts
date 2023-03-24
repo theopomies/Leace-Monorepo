252033d6a6491ec7ddff912f24903d64471bce12
Hugo Maltese <54075935+Lybertyxz@users.noreply.github.com>
2023-03-24 09:42:10 -0400

    Fix prod (#244)
    * fix ci/cd
    
    * delete oldmod
    
    * delete for fixing bug

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -1,7 +1,10 @@
+import { TRPCError } from "@trpc/server";
 import { publicProcedure, router } from "../trpc";
 
 export const authRouter = router({
   getSession: publicProcedure.query(({ ctx }) => {
-    return { auth: ctx.auth, role: ctx.role };
+    if (!ctx.auth || !ctx.auth.userId)
+      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+    return { auth: ctx.auth, userId: ctx.auth.userId, role: ctx.role };
   }),
 });
