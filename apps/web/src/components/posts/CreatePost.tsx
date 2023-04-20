0eec1bc121f214cfbb6277c443e2d497a3f8b7a0
Théo Pomies <theo.pomies@outlook.com>
2023-04-20 16:19:02 -0400

    NOT READY: Refacto Create Post

--- apps/web/src/components/posts/CreatePost.tsx
+++ apps/web/src/components/posts/CreatePost.tsx
@@ -3,6 +3,11 @@ import { Header } from "../../components/users/Header";
 import { useRouter } from "next/router";
 import { trpc, RouterInputs } from "../../utils/trpc";
 import { Button } from "../shared/button/Button";
+import { Input } from "../shared/forms/Input";
+import { Checkbox } from "../shared/forms/Checkbox";
+import { TextArea } from "../shared/forms/TextArea";
+import { NumberInput } from "../shared/forms/NumberInput";
+import { FileInput } from "../shared/forms/FileInput";
 
 export const CreatePost = () => {
   const router = useRouter();
@@ -82,253 +87,176 @@ export const CreatePost = () => {
   return (
     <div className="w-full">
       <Header heading={"Création d'une annonce"} />
-      <div className="flex justify-center p-5">
-        <div className="m-14 flex w-2/5 justify-center rounded-lg bg-white p-5 shadow">
-          <div className="h-auto p-4 text-center">
-            <div className="m-4">
-              <input
-                className="w-full rounded-lg border-2 border-gray-400 bg-gray-50 p-4 text-xl text-gray-900 focus:border-blue-600 focus:outline-none"
-                type="text"
-                placeholder="Titre de l'annonce"
-                name="title"
-                onChange={handleChange}
-                value={postData.title}
-              />
+      <div className="flex justify-center">
+        <div className="flex justify-center rounded-lg bg-white p-12 shadow">
+          <div className="h-auto">
+            <div className="flex w-full">
+              <label className="w-full">
+                <div className="text-lg font-medium">Title</div>
+                <Input
+                  placeholder="Appartment in Central Park"
+                  name="title"
+                  onChange={handleChange}
+                  value={postData.title}
+                  className="w-full"
+                />
+              </label>
             </div>
-            <div className="m-4 mt-10">
-              <input
-                className="w-3/4 rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none"
-                type="text"
-                placeholder="Dans quelle ville?"
-                name="location"
-                onChange={handleChange}
-                value={attributes.location}
-              />
+            <div className="mt-6 flex w-full">
+              <label className="w-full">
+                <div className="text-lg font-medium">Location</div>
+                <Input
+                  placeholder="Central Park, New York"
+                  name="location"
+                  onChange={handleChange}
+                  value={attributes.location}
+                  className="w-full"
+                />
+              </label>
             </div>
-            <div className="m-4">
-              <h2 className="pb-2 pt-4 text-xl font-medium text-gray-700">
+
+            <div className="mt-6">
+              <h2 className="text-center text-xl font-medium text-gray-700">
                 Type de logement
               </h2>
-              <label className="inline-flex items-center">
-                <input
+              <div className="flex justify-center gap-4 p-4">
+                <Checkbox
                   type="checkbox"
-                  className="form-checkbox"
                   name="house"
                   onChange={handleChange}
                   checked={attributes.house}
-                />
-                <span className="ml-2">Maison</span>
-              </label>
-              <label className="inline-flex items-center">
-                <input
+                >
+                  Maison
+                </Checkbox>
+                <Checkbox
                   type="checkbox"
-                  className="form-checkbox"
                   name="appartment"
                   onChange={handleChange}
                   checked={attributes.appartment}
-                />
-                <span className="ml-2">Appartement</span>
-              </label>
-            </div>
-            <div className="m-4">
-              <h2 className="pb-2 pt-4 text-xl font-medium text-gray-700">
-                Critères
-              </h2>
-              <div className="flex justify-between">
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="furnished"
-                    onChange={handleChange}
-                    checked={attributes.furnished}
-                  />
-                  <span className="ml-2">Fournis</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="pool"
-                    onChange={handleChange}
-                    checked={attributes.pool}
-                  />
-                  <span className="ml-2">Piscine</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="smoker"
-                    onChange={handleChange}
-                    checked={attributes.smoker}
-                  />
-                  <span className="ml-2">Fumer</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="terrace"
-                    onChange={handleChange}
-                    checked={attributes.terrace}
-                  />
-                  <span className="ml-2">Terasse</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="elevator"
-                    onChange={handleChange}
-                    checked={attributes.elevator}
-                  />
-                  <span className="ml-2">Ascenseur</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="parking"
-                    onChange={handleChange}
-                    checked={attributes.parking}
-                  />
-                  <span className="ml-2">Parking</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="garden"
-                    onChange={handleChange}
-                    checked={attributes.garden}
-                  />
-                  <span className="ml-2">Jardin</span>
-                </label>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox"
-                    name="pets"
-                    onChange={handleChange}
-                    checked={attributes.pets}
-                  />
-                  <span className="ml-2">Animaux</span>
-                </label>
+                >
+                  Appartement
+                </Checkbox>
               </div>
             </div>
-            <div className="m-8">
-              <textarea
-                className="w-full rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none"
-                rows={4}
+            <div>
+              <TextArea
                 placeholder="Description"
                 name="desc"
                 onChange={handleChange}
                 value={postData.desc}
+                className="w-full"
               />
             </div>
-            <div className="m-10">
-              <div className="m-4">
-                <div className="flex justify-center">
-                  <div className="relative rounded-md shadow-sm">
-                    <input
-                      type="number"
-                      className="form-input block w-full rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 py-3 pr-10 text-sm leading-5 text-gray-900 transition duration-150 ease-in-out focus:border-blue-600 focus:outline-none sm:text-sm sm:leading-5"
-                      placeholder="Surface"
-                      aria-describedby="amount-prefix"
-                      name="size"
-                      onChange={handleChange}
-                      value={attributes.size}
-                    />
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                      <span className="text-gray-500 sm:text-sm sm:leading-5">
-                        m²
-                      </span>
-                    </div>
-                    <label className="pl-3">Surface</label>
-                  </div>
-                </div>
+            <div className="mt-6">
+              <h2 className="text-center text-xl font-medium text-gray-700">
+                Critères
+              </h2>
+              <div className="flex flex-wrap justify-center gap-4 p-4">
+                <Checkbox
+                  name="furnished"
+                  onChange={handleChange}
+                  checked={attributes.furnished}
+                >
+                  Fournis
+                </Checkbox>
+                <Checkbox
+                  name="pool"
+                  onChange={handleChange}
+                  checked={attributes.pool}
+                >
+                  Piscine
+                </Checkbox>
+                <Checkbox
+                  name="smoker"
+                  onChange={handleChange}
+                  checked={attributes.smoker}
+                >
+                  Fumer
+                </Checkbox>
+                <Checkbox
+                  name="terrace"
+                  onChange={handleChange}
+                  checked={attributes.terrace}
+                >
+                  Terasse
+                </Checkbox>
+                <Checkbox
+                  name="elevator"
+                  onChange={handleChange}
+                  checked={attributes.elevator}
+                >
+                  Ascenseur
+                </Checkbox>
+                <Checkbox
+                  name="parking"
+                  onChange={handleChange}
+                  checked={attributes.parking}
+                >
+                  Parking
+                </Checkbox>
+                <Checkbox
+                  name="garden"
+                  onChange={handleChange}
+                  checked={attributes.garden}
+                >
+                  Jardin
+                </Checkbox>
+                <Checkbox
+                  name="pets"
+                  onChange={handleChange}
+                  checked={attributes.pets}
+                >
+                  Animaux
+                </Checkbox>
               </div>
             </div>
-            <div className="m-4">
-              <div className="flex justify-between">
-                <div className="relative rounded-md shadow-sm">
-                  <input
-                    type="number"
-                    className="form-input block w-full rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 py-3 pr-10 text-sm leading-5 text-gray-900 transition duration-150 ease-in-out focus:border-blue-600 focus:outline-none sm:text-sm sm:leading-5"
-                    placeholder="Prix"
-                    aria-describedby="amount-prefix"
-                    name="price"
-                    onChange={handleChange}
-                    value={attributes.price}
-                  />
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                    <span className="text-gray-500 sm:text-sm sm:leading-5">
-                      €
-                    </span>
-                  </div>
-                  <label className="pl-3">Prix du loyer</label>
-                </div>
-                <div className="relative rounded-md shadow-sm">
-                  <input
-                    type="number"
-                    className="form-input block w-full rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 py-3 pr-10 text-sm leading-5 text-gray-900 transition duration-150 ease-in-out focus:border-blue-600 focus:outline-none sm:text-sm sm:leading-5"
-                    placeholder="Charge"
-                    aria-describedby="amount-prefix"
-                    name="maxPrice"
-                  />
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                    <span className="text-gray-500 sm:text-sm sm:leading-5">
-                      €
-                    </span>
-                  </div>
-                  <label className="pl-3">Charge du loyer</label>
-                </div>
-              </div>
-              <h2 className="m-5 text-xl font-medium text-gray-700">Photos</h2>
-              <div className="flex justify-between">
-                <h2 className="pb-2 pt-4 text-xl font-medium text-gray-700">
-                  n°1:
-                </h2>
-                <label className="focus:shadow-outline-blue relative inline-flex items-center rounded-lg  border-2  border-gray-300  bg-gray-50 p-2.5 px-4 py-2  text-sm font-medium leading-5 text-gray-900 hover:text-gray-500  focus:border-blue-600  focus:outline-none active:bg-gray-50 active:text-gray-800">
-                  Upload
-                  <input
-                    hidden
-                    accept="image/*"
-                    multiple
-                    type="file"
-                    className="absolute inset-0 cursor-pointer opacity-0"
-                  />
-                </label>
-                <h2 className="pb-2 pt-4 text-xl font-medium text-gray-700">
-                  n°2:
-                </h2>
-                <label className="focus:shadow-outline-blue relative inline-flex items-center rounded-lg  border-2  border-gray-300  bg-gray-50 p-2.5 px-4 py-2  text-sm font-medium leading-5 text-gray-900 hover:text-gray-500  focus:border-blue-600  focus:outline-none active:bg-gray-50 active:text-gray-800">
-                  Upload
-                  <input
-                    hidden
-                    accept="image/*"
-                    multiple
-                    type="file"
-                    className="absolute inset-0 cursor-pointer opacity-0"
-                  />
-                </label>
-                <h2 className="pb-2 pt-4 text-xl font-medium text-gray-700">
-                  n°3:
-                </h2>
-                <label className="focus:shadow-outline-blue relative inline-flex items-center rounded-lg  border-2  border-gray-300  bg-gray-50 p-2.5 px-4 py-2  text-sm font-medium leading-5 text-gray-900 hover:text-gray-500  focus:border-blue-600  focus:outline-none active:bg-gray-50 active:text-gray-800">
-                  Upload
-                  <input
-                    hidden
-                    accept="image/*"
-                    multiple
-                    type="file"
-                    className="absolute inset-0 cursor-pointer opacity-0"
-                  />
-                </label>
+            <div className="mt-6 flex flex-wrap justify-around gap-4">
+              <label>
+                <div>Surface</div>
+                <NumberInput
+                  placeholder="Surface"
+                  aria-describedby="amount-prefix"
+                  name="size"
+                  onChange={handleChange}
+                  value={attributes.size}
+                  unit="m²"
+                />
+              </label>
+              <label>
+                <div>Prix du loyer</div>
+                <NumberInput
+                  placeholder="Prix"
+                  aria-describedby="amount-prefix"
+                  name="price"
+                  onChange={handleChange}
+                  value={attributes.price}
+                  unit="€"
+                />
+              </label>
+              <label>
+                <div>Charges</div>
+                <NumberInput
+                  placeholder="Charge"
+                  aria-describedby="amount-prefix"
+                  name="maxPrice"
+                  unit="€"
+                />
+              </label>
+            </div>
+            <div className="mt-6">
+              <h2 className="text-center text-xl font-medium text-gray-700">
+                Photos
+              </h2>
+              <div className="mt-2 flex flex-wrap justify-center gap-4">
+                <FileInput multiple={false}>Upload First Image</FileInput>
+                <FileInput multiple={false}>Upload Second Image</FileInput>
+                <FileInput multiple={false}>Upload Third Image</FileInput>
               </div>
             </div>
-            <div className="flex justify-center">
+            <div className="mt-6 flex justify-center gap-4">
+              <Button theme="danger" onClick={handleSubmit}>
+                Cancel
+              </Button>
               <Button onClick={handleSubmit}>Submit</Button>
             </div>
           </div>
