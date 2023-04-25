cd441c34188730c30942024b28270c6770946151
David <david.aphing-kouassi@epitech.eu>
2023-04-24 21:21:14 -0700

    [ADD] sign out button in dashboard for owners and agencies

--- apps/mobile/src/screens/Dashboard/dashboard.tsx
+++ apps/mobile/src/screens/Dashboard/dashboard.tsx
@@ -4,6 +4,24 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { TabStackParamList } from "../../navigation/TabNavigator";
 import { MotionButton } from "../../components/Button";
+import { Button } from "react-native-elements"
+import { useAuth } from "@clerk/clerk-expo";
+
+const SignOut = () => {
+    const { signOut } = useAuth();
+    return (
+        <View>
+            <Button
+                className="mx-9 mt-5 rounded bg-custom  text-white"
+                title="Sign Out"
+                buttonStyle={{ backgroundColor: "#002642" }}
+                onPress={() => {
+                    signOut();
+                }}
+            />
+        </View>
+    );
+};
 
 const Dashboard = () => {
 
@@ -12,9 +30,7 @@ const Dashboard = () => {
     return (
         <ScrollView className="mt-20">
             <View className="justify-center items-center p-3 pb-24 mt-10">
-
                 <Text className="text-center font-poppins text-3xl text-custom mx-auto">DASHBOARD</Text>
-
                 <View>
                     <View className="flex flex-wrap justify-center items-center p-3 pb-24 mt-10">
                         <MotionButton path={require("../../../assets/expenses.png")} label={"Expenses"} onPress={() => navigation.navigate("Expenses")} />
@@ -25,6 +41,7 @@ const Dashboard = () => {
                         <MotionButton path={require("../../../assets/chat.png")} label={"Chat"} onPress={() => navigation.navigate("Chat")} />
                     </View>
                 </View>
+                <SignOut />
             </View >
         </ScrollView >
     );
