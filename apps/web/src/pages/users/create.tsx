e0776ead5da722c0ab257e697c5b77bb5f51109c
Théo Pomies <theo.pomies@outlook.com>
2023-04-27 20:10:26 -0400

    fix users/create et signout

--- apps/web/src/pages/users/create.tsx
+++ apps/web/src/pages/users/create.tsx
@@ -1,14 +1,28 @@
-import { useSession } from "@clerk/nextjs";
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  useSession,
+} from "@clerk/nextjs";
 import { Loader } from "../../components/shared/Loader";
 import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function CreateUser() {
   const session = useSession();
   const createUser = trpc.user.createUser.useMutation();
   const router = useRouter();
   const [redirecting, setRedirecting] = useState(false);
+
+  const from = useMemo(() => {
+    const from = router.query.from;
+    if (typeof from === "string") {
+      // Decode the url encoded string
+      return decodeURIComponent(from);
+    }
+    return "/";
+  }, [router.query.from]);
 
   useEffect(() => {
     if (!session.isLoaded || !session || redirecting || !session.isSignedIn) {
@@ -22,18 +36,25 @@ export default function CreateUser() {
         console.error(err);
       })
       .then(() => {
-        router.push("/"); // Redirect to home page
+        router.push(from); // Redirect to home page
       });
-  }, [session, createUser, router, redirecting]);
+  }, [session, createUser, router, redirecting, from]);
 
   if (!session.isLoaded) {
     return <Loader />;
   }
 
-  if (!session.isSignedIn) {
-    router.push("/sign-in");
-    return <Loader />;
-  }
-
-  return <Loader />;
+  return (
+    <>
+      <SignedIn>
+        <Loader />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn
+          afterSignUpUrl={"/users/create"}
+          afterSignInUrl={router.asPath}
+        />
+      </SignedOut>
+    </>
+  );
 }
