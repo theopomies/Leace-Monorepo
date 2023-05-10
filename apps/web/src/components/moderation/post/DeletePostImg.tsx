bf1d1e30e3d501464994ce91b275f7f010742d4d
Romain Dufourt <romain.dufourt@epitech.eu>
2023-05-10 15:10:03 -0400

    Fix CI

--- apps/web/src/components/moderation/post/DeletePostImg.tsx
+++ apps/web/src/components/moderation/post/DeletePostImg.tsx
@@ -9,12 +9,10 @@ export interface DeletePostImgProps {
 }
 
 export const DeletePostImg = ({ postId, id }: DeletePostImgProps) => {
-  const utils = trpc.useContext();
-  const mut = trpc.moderation.image.deleteSignedPostUrl.useMutation();
+  const mut = trpc.image.deleteSignedPostUrl.useMutation();
   const onClickDelete = async () => {
-    await mut.mutateAsync({ postId: postId, id: id }).then(async (url) => {
+    await mut.mutateAsync({ postId: postId, imageId: id }).then(async (url) => {
       await axios.delete(url);
-      utils.image.getSignedUserUrl.refetch();
     });
   };
 
