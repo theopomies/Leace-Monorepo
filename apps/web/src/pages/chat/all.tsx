6a1ad1e1a2dc5d0d2b0fe7940fa3b6d09f2439e9
Herosbrine <gregoiretheboss@gmail.com>
2023-05-20 16:57:11 +0200

    [CHAT] | working perfectly

--- apps/web/src/pages/chat/all.tsx
+++ apps/web/src/pages/chat/all.tsx
@@ -2,15 +2,22 @@ import { Chat } from "../../components/shared/chat";
 import { trpc } from "../../utils/trpc";
 import { Loader } from "../../components/shared/Loader";
 import { LoggedLayout } from "../../components/layout/LoggedLayout";
+import { Role } from "@prisma/client";
 
 const ChatAll = () => {
   const session = trpc.auth.getSession.useQuery();
+  const role = session.data?.role;
 
   if (session.data && !session.error) {
     return (
       <LoggedLayout title="Chat | Leace">
         <div className="flex h-screen w-full justify-center p-4">
-            <Chat userId={session.data.userId} chatOn/>
+          {role === Role.TENANT && (
+            <Chat userId={session.data.userId} chatOn isTenant={true} />
+          )}
+          {(role === Role.AGENCY || role === Role.OWNER) && (
+            <Chat userId={session.data.userId} chatOn isTenant={false} />
+          )}
         </div>
       </LoggedLayout>
     );
