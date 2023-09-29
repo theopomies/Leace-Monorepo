13a08e14a8a2405300b125669529fc62782d275d
Carlos <carlos.frangeard@epitech.eu>
2023-09-29 18:49:35 +0200

    [ADD] implemented tailwind

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ apps/mobile/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  presets: [require("@leace/tailwind-config")],
+  theme: {
+    extend: {
+      colors: {
+        custom: "#002642",
+        navy: "#10316B",
+      },
+    },
+  },
+};
