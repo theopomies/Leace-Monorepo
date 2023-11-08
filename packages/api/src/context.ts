7aae1d90291b6b777e88e20dd456cd08343ddfd8
Hugo Maltese <hugo.maltese@epitech.eu>
2023-11-08 13:30:55 +0100

    why

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -6,7 +6,7 @@ import { getAuth, createClerkClient, clerkClient } from "@clerk/nextjs/server";
 import type {
   SignedInAuthObject,
   SignedOutAuthObject,
-} from "@clerk/backend/dist/types/tokens/authObjects";
+} from "@clerk/nextjs/api";
 import mixpanel, { Mixpanel } from "mixpanel";
 /**
  * Replace this with an object if you want to pass things to createContextInner
