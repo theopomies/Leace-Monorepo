e0cead5698b029aa772397131ab0a77e57504f4e
Théo Pomies <theo.pomies@outlook.com>
2023-11-16 15:01:52 +0100

    Fix roles protection

--- apps/web/src/pages/posts/[postId]/index.tsx
+++ apps/web/src/pages/posts/[postId]/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import { LoggedLayout } from "../../../components/layout/LoggedLayout";
 import { Post } from "../../../components/posts/Post";
+import { Role } from "@prisma/client";
 
 const Index = () => {
   const router = useRouter();
@@ -12,7 +13,7 @@ const Index = () => {
   }
 
   return (
-    <LoggedLayout title="Post | Leace">
+    <LoggedLayout title="Post | Leace" roles={[Role.AGENCY, Role.OWNER]}>
       <div className="flex flex-grow overflow-hidden p-10">
         <Post postId={postId} />
       </div>
