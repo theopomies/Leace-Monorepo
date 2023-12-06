b0010cd6a60d07ec0c65409d528a6ebf864f69f5
Hugo Humez <hugo.humez@epitech.eu>
2023-12-06 18:36:22 +0100

    Adding new design for Choose attributes + Fix effect HouseType buttong

--- apps/mobile/src/screens/OnBoarding/ChooseAttributes.tsx
+++ apps/mobile/src/screens/OnBoarding/ChooseAttributes.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import { IStep } from "../../types/onboarding";
 import { IUserAttrs } from "../../types";
 import { EditAttributes } from "../../components/Attribute";
 import { Btn } from "../../components/Btn";
 import { trpc } from "../../utils/trpc";
-
+import EditAttributesRefacto from "../../components/Attribute/EditAttributesRefacto";
 export default function ChooseAttributes({
   userId,
   setStep,
@@ -21,32 +21,39 @@ export default function ChooseAttributes({
 
   return (
     <>
-      <View className="flex h-40 flex-col justify-center px-8">
-        <Text className="text-3xl font-bold">
-          One last thing, tell us what you are looking for!
-        </Text>
-      </View>
-      <View className="flex-1 px-8">
-        <EditAttributes userId={"userId"} attrs={attrs} setAttrs={setAttrs} />
-      </View>
-      <View className="flex flex-row justify-between px-8 pb-4">
-        <Btn
-          title="Back"
-          onPress={() => {
-            setProgress(50);
-            setStep("PROFILE");
-          }}
-        />
-        <Btn
-          title="Finish setting me up"
-          onPress={() => {
-            userAttributes.mutate({
-              userId,
-              ...attrs,
-            });
-          }}
-        />
-      </View>
+      <ScrollView>
+
+        <View className="flex h-40 flex-col justify-center px-8">
+          <Text className="text-3xl font-bold">
+            One last thing, tell us what you are looking for!
+          </Text>
+        </View>
+        <View className="flex-1 px-8 pb-4">
+          <EditAttributesRefacto userId={"userId"} attrs={attrs} setAttrs={setAttrs} onBoarding={true} />
+        </View>
+        <View className="flex flex-row justify-between px-8 pb-4">
+          <Btn
+            title="Back"
+            bgColor="#6C47FF"
+            onPress={() => {
+              setProgress(50);
+              setStep("PROFILE");
+            }}
+          />
+          <Btn
+            title="Finish setting me up"
+            bgColor="#6C47FF"
+            onPress={() => {
+              if (!attrs?.homeType)
+                attrs!.homeType = "HOUSE"
+              userAttributes.mutate({
+                userId,
+                ...attrs,
+              });
+            }}
+          />
+        </View>
+      </ScrollView>
     </>
   );
 }
