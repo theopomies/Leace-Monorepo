b065b1521cbb8a942ff62e4ef5d792dcf76d9975
Théo Pomies <theo.pomies@outlook.com>
2024-01-18 06:58:43 +0100

    fix: negative numbers, 0 allowed sometimes and no decimals

--- apps/web/src/components/shared/forms/NumberInput.tsx
+++ apps/web/src/components/shared/forms/NumberInput.tsx
@@ -5,7 +5,23 @@ export type NumberInputProps = InputProps & { unit?: string };
 export function NumberInput({ unit, ...props }: NumberInputProps) {
   return (
     <div className="relative inline-block">
-      <Input {...props} type="number" />
+      <Input
+        min={0}
+        // only allow integers
+        pattern="\d*"
+        {...props}
+        onChange={(e) => {
+          const value = e.target.valueAsNumber;
+          if (isNaN(value)) {
+            props.onChange?.(e);
+            return;
+          }
+          e.target.value = Math.round(value).toString();
+          e.target.valueAsNumber = Math.round(value);
+          props.onChange?.(e);
+        }}
+        type="number"
+      />
       {unit && (
         <div className="absolute right-6 top-0 flex h-full items-center px-2">
           {unit}
